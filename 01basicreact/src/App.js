71113140d676704577dd84508304576181ef17eb
AmitPrasad212003 <amitprasad212003@gamil.com>
2025-03-17 03:16:54 +0530

    create a basic react app by different ways

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Hello amit bhai</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  
+
+  return (
+    <h1>Hello Bahiya ji </h1>
+  )
+}
+
+export default App
